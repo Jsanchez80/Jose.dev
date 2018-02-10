c0a83afdafa1c114241566122a2202c6d7a50152
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2018-02-09 21:20:26 -0600

    Adding javascript to the project to verify user. Function verifyUser() was created and should fire off accordingly.  If successful login, a new message welcoming the guest will be  generated. If login is unsuccessful, a message asking the guest to sign in again will be generated.

--- public/js/iLogin.js
+++ public/js/iLogin.js
@@ -1,9 +1,12 @@
-var username = "Luis";
+var username = "iWorks";
 var password = "password";
 
 
-if (username == "iWorks" && password == "password") {
-	getElementById("login-message").textContent = "Welcome back " + username +"!";
-} else {
-	getElementById("login-message").textContent = "Unfortunately, We cannot identify you. Please re enter your username and password.";
-}
+	function verifyUser() {
+		if (username == "iWorks" && password == "password") {
+		getElementById("login-message").textContent = "Welcome back " + username +"!";
+		} else {
+		getElementById("login-message").textContent = "Unfortunately, We cannot identify you. Please re enter your username and password.";
+		}
+
+	}
